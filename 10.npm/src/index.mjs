8c10332872d452ffe8665b24dc874a4c7f5e86d5
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2023-05-22 21:52:43 +0200

    config webpack

---
+++ 10.npm/src/index.mjs
@@ -0,0 +1,6 @@
+import { sum } from "./sum.mjs";
+import { sub } from "./sub.cjs";
+
+console.log(sub(1, 2));
+
+export {};
--- 9.webpack/webpack.config.js
+++ 9.webpack/webpack.config.js
@@ -1,10 +1,26 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: 'production',
+  entry: "./src/tic-tac-toe.ts",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "output.js",
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
   },
 };
